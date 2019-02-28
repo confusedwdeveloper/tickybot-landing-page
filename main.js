8791f0a6db7fdcef598dd7e228da800df4391f83
Darshan Datta <darshandatta.10@gmail.com>
2019-03-01 00:22:23 +0530

    js scroll into view animation done

--- main.js
+++ main.js
@@ -14,3 +14,6 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// scroll reveal
+ScrollReveal().reveal('.scroll-view', { delay: 500 });
