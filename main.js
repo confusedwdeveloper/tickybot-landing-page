91da653b3d073cd62f728e3a58a7a80774013571
Darshan Datta <darshandatta.10@gmail.com>
2019-03-01 11:48:05 +0530

    javascript portion complete

--- main.js
+++ main.js
@@ -17,3 +17,20 @@ function topFunction() {
 
 // scroll reveal
 ScrollReveal().reveal('.scroll-view', { delay: 500 });
+
+
+// smooth scrolling for all anchor links
+let anchorlinks = document.querySelectorAll('a[href^="#"]')
+
+for (let item of anchorlinks) { // relitere
+    item.addEventListener('click', (e)=> {
+        let hashval = item.getAttribute('href')
+        let target = document.querySelector(hashval)
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+        history.pushState(null, null, hashval)
+        e.preventDefault()
+    })
+}
